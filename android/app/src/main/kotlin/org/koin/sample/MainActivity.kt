119d2812e80547bc6107f89a86f763ffecde8f18
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2022-10-01 11:27:25 +0200

    clean up

--- android/app/src/main/kotlin/org/koin/sample/MainActivity.kt
+++ android/app/src/main/kotlin/org/koin/sample/MainActivity.kt
@@ -1,10 +1,9 @@
 package org.koin.sample
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import fr.ekito.myweatherapp.R
-import kotlinx.android.synthetic.main.activity_simple.*
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.sample.data.DefaultData
@@ -14,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     private val presenter: UserPresenter by inject()
     private val viewModel : UserViewModel by viewModel()
 
-    @SuppressLint("SetTextI18n") // just for the text format
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple)
@@ -22,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         title = "MySimpleActivity"
 
         val name = DefaultData.DEFAULT_USER.name
-        text.text = "${presenter.sayHello(name)} \n ${viewModel.sayHello(name)}"
+        val textField = findViewById<TextView>(R.id.text)
+        textField.text = "${presenter.sayHello(name)} \n ${viewModel.sayHello(name)}"
     }
 }
