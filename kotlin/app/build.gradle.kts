e5c988e38636791802d085001929073bb0508d88
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2024-10-16 19:04:23 +0200

    kotlin standalone

---
+++ kotlin/app/build.gradle.kts
@@ -0,0 +1,39 @@
+val koinVersion = "4.0.0"
+
+plugins {
+    val kotlinVersion = "2.0.20"
+    // Apply the Kotlin JVM plugin
+    kotlin("jvm") version kotlinVersion
+    // Apply the application plugin for CLI application support
+    application
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    // Use the Kotlin JDK 8 standard library
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Koin dependencies
+    implementation("io.insert-koin:koin-core:$koinVersion")
+
+    // Koin testing
+    testImplementation("io.insert-koin:koin-test:$koinVersion")
+    testImplementation("io.insert-koin:koin-test-junit4:$koinVersion")
+    testImplementation("org.mockito:mockito-inline:4.8.0")
+}
+
+application {
+    // Define the main class for the application
+    mainClass.set("org.koin.sample.UserApplication")
+}
+
+java {
+    // Use Java 17 for compilation
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
--- kotlin/app/src/test/kotlin/org/koin/sample/ModuleVerificationTest.kt
+++ kotlin/app/src/test/kotlin/org/koin/sample/ModuleVerificationTest.kt
@@ -1,18 +1,13 @@
 package org.koin.sample
 
 import org.junit.Test
-import org.junit.experimental.categories.Category
-import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.sample.di.appModule
 import org.koin.test.AutoCloseKoinTest
-import org.koin.test.category.CheckModuleTest
 import org.koin.test.verify.verify
 
 /**
  * Dry run configuration
  */
-@Category(CheckModuleTest::class)
-@OptIn(KoinExperimentalAPI::class)
 class ModuleVerificationTest : AutoCloseKoinTest() {
 
     @Test
