282e2928ff818e8e57292e5727c5826d925d6ac0
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2024-10-21 15:34:59 +0200

    remove old kmp project

--- kmp/androidApp/src/main/java/org/koin/sample/di/AndroidModule.kt
+++
@@ -1,9 +0,0 @@
-package org.koin.sample.di
-
-import org.koin.sample.AndroidUserViewModel
-import org.koin.androidx.viewmodel.dsl.viewModelOf
-import org.koin.dsl.module
-
-val androidModule = module {
-    viewModelOf(::AndroidUserViewModel)
-}
